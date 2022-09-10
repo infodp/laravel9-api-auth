0dfcc03d88dd391cb49bc8ecbce6773e1a0dd553
InfoDP <infodpdev@gmail.com>
2022-09-10 18:08:06 -0300

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        //validación de los datos
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);    
+        //alta del usuario
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        //respuesta
+        /* return response()->json([
+            "message" => "Alta exitosa"
+        ]); */
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            $cookie = cookie('cookie_token', $token, 60 * 24);
+            return response(["token"=>$token], Response::HTTP_OK)->withoutCookie($cookie);
+        } else {
+            return response(["message"=> "Credenciales inválidas"],Response::HTTP_UNAUTHORIZED);
+        }        
+    }
+
+    public function userProfile(Request $request) {
+        return response()->json([
+            "message" => "userProfile OK",
+            "userData" => auth()->user()
+        ], Response::HTTP_OK);
+    }
+    
+    public function logout() {
+        $cookie = Cookie::forget('cookie_token');
+        return response(["message"=>"Cierre de sesión OK"], Response::HTTP_OK)->withCookie($cookie);
+    }
+
+    public function allUsers() {
+       $users = User::all();
+       return response()->json([
+        "users" => $users
+       ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('user-profile', [AuthController::class, 'userProfile']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::get('users', [AuthController::class, 'allUsers']);
